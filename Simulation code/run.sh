0b4694515e707e6ab637383a9a76430ec92382ee
YP kuo <yangpingkuo@gmail.com>
2023-11-08 10:51:41 -0500

    Update run.sh

--- Simulation code/run.sh
+++ Simulation code/run.sh
@@ -3,4 +3,4 @@ g++ -std=c++11 clonal.cpp -o clonal
 
 # running the executable
 # input: graph in edge list representation, output file, number of runs, mutation rate, selection on the intermediate mutatnt
-./clonal graphs/edgelist.txt results/out.txt 10000000 2e-6 -0.002
+./Crossing graphs/edgelist.txt results/out.txt 10000000 2e-6 -0.002
