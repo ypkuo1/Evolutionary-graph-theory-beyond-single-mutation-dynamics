7a02ad2cf7ce02b3d9bb7a972c4b725571938d3b
YP kuo <yangpingkuo@gmail.com>
2024-01-10 12:28:14 -0500

    Delete Simulation code/run.sh

--- Simulation code/run.sh
+++
@@ -1,6 +0,0 @@
-# compiling the code
-g++ -std=c++11 Crossing.cpp -o Crossing
-
-# running the executable
-# input: graph in edge list representation, output file, number of runs, mutation rate, selection on the intermediate mutatnt
-./Crossing graphs/edgelist.txt results/out.txt 10000000 2e-6 -0.002
